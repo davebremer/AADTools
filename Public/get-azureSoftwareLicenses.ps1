39c35d80aac37ae0fb66b8174559454f411ff642
Dave Bremer <dave@bremer.co.nz>
2020-04-17 15:50:01 +1200

    added get-azuresoftwarelicenses

---
+++ Public/get-azureSoftwareLicenses.ps1
@@ -0,0 +1,33 @@
+﻿function get-azureSoftwareLicenses {
+<#
+.SYNOPSIS
+   Returns the license count from Azure.
+ 
+
+.DESCRIPTION
+ Returns the license count from Azure. Really just a wrapper for Get-MsolAccountSku from module msolservice
+
+.EXAMPLE
+ azueSoftwareLicenses
+ Returns for each Azuer software license: skupartnumber, ActiveUnits,ConsumedUnits,WarningUnits
+ 
+.NOTES
+ Author: Dave Bremer
+ Date: 
+ Revisions:
+    
+
+#>
+
+#Check for a connection
+
+try {
+    Write-Verbose "Already connected"
+    Get-MsolDomain -ErrorAction Stop > $null
+} Catch {
+    Write-Output "Connecting to Azure..."
+    Connect-MsolService
+}
+
+Get-MsolAccountSku | select skupartnumber,ActiveUnits,ConsumedUnits,WarningUnits
+}
